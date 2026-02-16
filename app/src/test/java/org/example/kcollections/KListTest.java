846e5fdc55aadfa8756f66c5f1c7ba1d0ecffaf5
sebastian.schmidt <bastman77@gmail.com>
2026-02-16 11:47:49 +0100

    kcollections (java port of kotlin collections)

--- app/src/test/java/org/example/kcollections/KListTest.java
+++ app/src/test/java/org/example/kcollections/KListTest.java
@@ -1,23 +1,27 @@
-package org.example;
+package org.example.kcollections;
 
 import org.junit.jupiter.api.Test;
+
 class KListTest {
 
     record User(
             String name, Integer age, String countryCode
-    ){};
+    ) {
+    }
+
+    ;
 
     @Test
     void foo1() {
         var users = java.util.List.of(
                 new User("u_001", 10, "DE"),
-                new User("u_002",19, "DE"),
-                new User("u_003",20, "DE"),
+                new User("u_002", 19, "DE"),
+                new User("u_003", 20, "DE"),
                 new User("u_101", 10, "US"),
-                new User("u_102",19, "US"),
-                new User("u_103",19, "US")
+                new User("u_102", 19, "US"),
+                new User("u_103", 19, "US")
         );
-        var result = org.example.kcollections.KList.of(users)
+        var result = KList.of(users)
                 .filter(u -> u.age > 18)
                 .map(User::name)
                 .groupBy(name -> name.charAt(0))
@@ -30,13 +34,13 @@ class KListTest {
     void foo2() {
         var users = java.util.List.of(
                 new User("u_001", 10, "DE"),
-                new User("u_002",19, "DE"),
-                new User("u_003",20, "DE"),
+                new User("u_002", 19, "DE"),
+                new User("u_003", 20, "DE"),
                 new User("u_101", 10, "US"),
-                new User("u_102",19, "US"),
-                new User("u_103",19, "US")
+                new User("u_102", 19, "US"),
+                new User("u_103", 19, "US")
         );
-        var result = org.example.kcollections.KList.of(users)
+        var result = KList.of(users)
                 .filter(u -> u.age > 18)
                 .map(User::name)
                 .groupBy(name -> name.charAt(0))
